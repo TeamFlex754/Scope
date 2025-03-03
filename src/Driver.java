cf8c065b821728a880079a9b5253105786733d12
iansm <ians.mcvay@gmail.com>
2025-03-02 20:21:49 -0800

    initial commit

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Driver {
+
+    // instance variable declared within class but outside method
+    int instanceVariable = 20;
+
+    public static void main(String[] args) {
+
+        // local variable x declared within the main method
+        int x = 10;
+        System.out.println("Local Variable: " + x);
+
+        // create Driver object to access and print the instance variable
+        Driver obj = new Driver();
+        System.out.println("Instance Variable: " + obj.instanceVariable);
+
+        // call the add method with arguments 5 and 10
+        int addition = add(5,10);
+        System.out.println("Sum of parameters: num1 + num2 = " + addition);
+    }
+
+    public static int add(int num1, int num2) {
+        // local variable sum declared within add method
+        int sum = 0;
+        sum = num1 + num2;
+        return sum;
+    }
 }
